c8b2162e94212b616c7651a7072d0f7a2e5d6417
gunjansingh8533 <gunjansingh8533@gmail.com>
2026-06-27 12:01:34 +0530

    Time: 520 ms (33.54%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT id,
     (CASE WHEN p_id IS NULL THEN "Root"
-         WHEN id IN (SELECT p_id FROM Tree) AND p_id IS NOT NULL THEN "Inner"
+         WHEN id IN (SELECT p_id FROM Tree) THEN "Inner"
          ELSE "Leaf"
          END) AS type
 FROM Tree
