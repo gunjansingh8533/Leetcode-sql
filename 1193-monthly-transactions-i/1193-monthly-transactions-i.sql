3da31dcc6f873c66c1b170fc392b33e228c42d4c
gunjansingh8533 <gunjansingh8533@gmail.com>
2026-09-09 14:32:44 +0530

    Time: 3912 ms (5%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -2,8 +2,8 @@
 SELECT SUBSTRING(trans_date, 1, 7) AS month,
        country,
        COUNT(*) AS trans_count,
-       SUM(CASE WHEN state = "approved" THEN 1 ELSE 0 END) AS approved_count,
+       COUNT(CASE WHEN state = "approved" THEN 1 END) AS approved_count,
        SUM(amount) AS trans_total_amount,
-       IFNULL(SUM(CASE WHEN state = "approved" THEN amount END), 0) AS approved_total_amount
+       SUM(CASE WHEN state = "approved" THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
-GROUP BY country, month;
+GROUP BY month, country;
